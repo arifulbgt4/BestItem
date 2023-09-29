0a001bc7694ec5118d162769b8c2ac85e6235506
devcowsar <kawsarcse836@gmail.com>
2023-09-29 21:01:22 +0600

    feat: Home layout

---
+++ src/widgets/ContentList/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { ContentListProps } from "./Types";
+
+const ContentList: FC<ContentListProps> = () => {
+  return <div>ContentList</div>;
+};
+
+export default ContentList;
---
+++ src/widgets/HeroBanner/inedex.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { HeroBannerProps } from "./Types";
+
+const HeroBanner: FC<HeroBannerProps> = () => {
+  return <div>banner</div>;
+};
+
+export default HeroBanner;
