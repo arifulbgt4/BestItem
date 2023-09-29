5133c2e528001d2a8f2a94a041a870b8383e7fe6
devcowsar <kawsarcse836@gmail.com>
2023-09-29 21:31:47 +0600

    feat: layout porduct detaile page

--- src/app/[locale]/product/[slug]/page.tsx
+++ src/app/[locale]/product/[slug]/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Grid } from "@mui/material";
+
+import ProductDeatailsBanner from "src/widgets/ProductDetailsBanner";
+import ProductDetailsCard from "src/widgets/ProductDetailsCard";
 
 const ProdutPage = () => {
-  return <div>ProdutPage</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <ProductDeatailsBanner />
+      </Grid>
+      <Grid item xs={12}>
+        <ProductDetailsCard />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ProdutPage;
---
+++ src/widgets/ProductDetailsBanner/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { ProductDetailsBannerProps } from "./Types";
+
+const ProductDeatailsBanner: FC<ProductDetailsBannerProps> = () => {
+  return <div>ProductDeatailsBanner</div>;
+};
+
+export default ProductDeatailsBanner;
