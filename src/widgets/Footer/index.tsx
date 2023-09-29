3e6c8bd77b6e55420f70d1ce9d2061bc8c2a977b
devcowsar <kawsarcse836@gmail.com>
2023-09-30 01:33:38 +0600

    upgrade: footer

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,8 +1,23 @@
-import React, { FC } from "react";
+import { FC } from "react";
+
 import { FooterProps } from "./Types";
+import { Stack, Typography } from "@mui/material";
 
 const Footer: FC<FooterProps> = () => {
-  return <div>Footer</div>;
+  return (
+    <Stack
+      direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
+      bgcolor="grey"
+      alignItems="center"
+      justifyContent="space-around"
+      p={2}
+      color="#b7aeae"
+    >
+      <Typography>@2023 www.product.com</Typography>
+      <Typography>About us</Typography>
+      <Typography>Support</Typography>
+    </Stack>
+  );
 };
 
 export default Footer;
