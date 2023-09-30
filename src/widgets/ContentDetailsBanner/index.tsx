defeb66572611de068591f4bb3ffaf46ef6a91d7
devcowsar <kawsarcse836@gmail.com>
2023-10-01 01:31:44 +0600

    fix: contentDetails banner

--- src/widgets/ContentDetailsBanner/index.tsx
+++ src/widgets/ContentDetailsBanner/index.tsx
@@ -12,22 +12,20 @@ import { ContentDetailsBannerProps } from "./Types";
 
 const ContentDeatailsBanner: FC<ContentDetailsBannerProps> = () => {
   return (
-    <Container>
-      <Card>
-        <CardMedia
-          sx={{ height: "300px" }}
-          image="https://s40424.pcdn.co/in/wp-content/uploads/2022/04/March_How-to-become-Product-Manager-1.jpg.optimal.jpg"
-        >
-          <Typography variant="h3">Products details banner</Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
-            ipsam consectetur dolorum delectus, deserunt repellat molestiae
-            dolor necessitatibus cum libero quam maxime tempora nostrum quasi
-            quibusdam excepturi earum quas sed?
-          </Typography>
-        </CardMedia>
-      </Card>
-    </Container>
+    <Card>
+      <CardMedia
+        sx={{ height: "300px" }}
+        image="https://s40424.pcdn.co/in/wp-content/uploads/2022/04/March_How-to-become-Product-Manager-1.jpg.optimal.jpg"
+      >
+        <Typography variant="h3">Products details banner</Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
+          ipsam consectetur dolorum delectus, deserunt repellat molestiae dolor
+          necessitatibus cum libero quam maxime tempora nostrum quasi quibusdam
+          excepturi earum quas sed?
+        </Typography>
+      </CardMedia>
+    </Card>
   );
 };
 
