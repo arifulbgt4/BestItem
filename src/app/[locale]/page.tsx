d0f4c0ffdccc06ce6de7822e956d509dde5081a0
devcowsar <kawsarcse836@gmail.com>
2023-10-01 01:11:44 +0600

    fix:hero banner

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { Grid } from "@mui/material";
 import { useTranslations } from "next-intl";
 
 import ContentItem from "src/widgets/ContentItem";
-import HeroBanner from "src/widgets/HeroBanner/inedex";
+import HeroBanner from "src/widgets/HeroBanner";
 
 export default function Home() {
   // const t = useTranslations();
--- src/widgets/HeroBanner/index.tsx
+++ src/widgets/HeroBanner/index.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
-import { Grid, Typography } from "@mui/material";
+import { Paper, Box, Typography } from "@mui/material";
 import { HeroBannerProps } from "./Types";
 
 const HeroBanner: FC<HeroBannerProps> = () => {
   return (
-    <Grid container p={8} justifyContent="center">
-      <Grid item>
+    <Paper elevation={4} sx={{ padding: 8, my: 1 }}>
+      <Box display="flex" flexDirection="column" alignItems="center">
         <Typography variant="h3">We Helps You</Typography>
         <Typography variant="h4">Find Your Best Product</Typography>
-      </Grid>
-    </Grid>
+      </Box>
+    </Paper>
   );
 };
 
