db4a92f9e0a391c15722c7e8f25fee2edcccc472
devcowsar <kawsarcse836@gmail.com>
2023-10-02 01:21:58 +0600

    refactor: clean code

--- src/widgets/ProductDetailsCard/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { ProductDetailsCardProps } from "./Types";
-
-const ProductDetailsCard: FC<ProductDetailsCardProps> = () => {
-  return <div>ProductDetailsCard</div>;
-};
-
-export default ProductDetailsCard;
