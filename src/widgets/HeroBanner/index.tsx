85361c14c47aab2c1d93028e295fe874a0c909c2
devcowsar <kawsarcse836@gmail.com>
2023-10-02 00:58:12 +0600

    fix: herobanner

--- src/widgets/HeroBanner/index.tsx
+++ src/widgets/HeroBanner/index.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
-import { Paper, Box, Typography } from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
 
 import { HeroBannerProps } from "./Types";
 
 const HeroBanner: FC<HeroBannerProps> = () => {
   return (
-    <Paper elevation={4} sx={{ padding: 8, my: 1 }}>
-      <Box display="flex" flexDirection="column" alignItems="center">
+    <Stack direction="column" alignItems="center">
+      <Box>
         <Typography variant="h3">We Helps You</Typography>
+      </Box>
+      <Box>
         <Typography variant="h4">Find Your Best Product</Typography>
       </Box>
-    </Paper>
+    </Stack>
   );
 };
 
