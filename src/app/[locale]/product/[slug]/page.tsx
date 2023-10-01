1c3d1a85d05db371a67fb0171a089bf0793a5e4c
devcowsar <kawsarcse836@gmail.com>
2023-10-02 00:49:14 +0600

    fix: contentdetails banner props

--- src/app/[locale]/product/[slug]/page.tsx
+++ src/app/[locale]/product/[slug]/page.tsx
@@ -7,7 +7,11 @@ const ProdutPage = () => {
   return (
     <Grid container>
       <Grid item xs={12}>
-        <ContentDeatailsBanner />
+        <ContentDeatailsBanner
+          title="Title"
+          subtitle="subtitle"
+          image="https://s40424.pcdn.co/in/wp-content/uploads/2022/04/March_How-to-become-Product-Manager-1.jpg.optimal.jpg"
+        />
       </Grid>
       <Grid item xs={12}>
         <ProductDetailsCard />
